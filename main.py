be9c087657240e1b9de2a5e948f8053d31ea6286
Morshheiser <anselmiezequiel85@gmail.com>
2024-07-03 12:48:14 -0300

    organizando melhor o repositorio

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
-from extract_xml import process_all_xml_files
-from extract_xlsx import process_all_xlsx_files
-from ship_union import combine_csv_files
-from ship_id import separate_by_ship
-from csv_column_processor import  process_column_in_csv
+import sys
+
+# Adicione o diretório ao sys.path
+sys.path.append(r'C:\Users\morsh\Desktop\Casa DIgital\Data_Science_Project_-_Challenge_2\all_files_py')
+
+# Importe os módulos
+try:
+    from extract_xml import process_all_xml_files
+    from extract_xlsx import process_all_xlsx_files
+    from ship_union import combine_csv_files
+    from ship_id import separate_by_ship
+    from csv_column_processor import process_column_in_csv
+except ImportError as e:
+    print(f"Erro ao importar os módulos: {e}")
+    sys.exit(1)
 
 def main():
-    # Diretório de entrada contendo os arquivos XML
-    input_directory = r'C:\Users\morsh\Desktop\Casa DIgital\projeto de ciencias de dados\Desafio_Log'
-
+    # Diretório de entrada contendo os arquivos XML e xlsl
+    input_directory = r'C:\Users\morsh\Desktop\Casa DIgital\Data_Science_Project_-_Challenge_2\all_files'
+    # Diretório de entrada para os arquivos CSV
+    input_directory_2 = r'C:\Users\morsh\Desktop\Casa DIgital\Data_Science_Project_-_Challenge_2\all_files\ship_project'
     # Diretório de saída para os arquivos CSV
-    output_directory = r'C:\Users\morsh\Desktop\Casa DIgital\projeto de ciencias de dados\Desafio_Log\ship_project'
+    output_directory = r'C:\Users\morsh\Desktop\Casa DIgital\Data_Science_Project_-_Challenge_2\all_files\ship_project'
 
     try:
         # Processa todos os arquivos XML e XLSX
@@ -19,7 +30,7 @@ def main():
         
         # Diretório onde os arquivos CSV foram salvos
         csv_directory = output_directory
-        process_column_in_csv(input_directory)
+        process_column_in_csv(input_directory_2)
         # Combina todos os arquivos CSV
         combine_csv_files(csv_directory)
         
